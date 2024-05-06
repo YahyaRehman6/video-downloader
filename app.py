c09e7ba450ea8f9ff03792ce112e517b2d699d09
Usman Shabbir <ushabbir.se@gmail.com>
2024-05-06 18:23:44 +0500

    youtube api

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 
 import re
 import requests 
+from pytube import YouTube
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -79,3 +80,58 @@ def tiktok():
         return jsonify({
             'error': str(e) 
         })
+
+def get_youtube_detail(video_url):
+    try:
+        # Create a YouTube object with the given video URL
+        yt = YouTube(video_url)
+
+        # Get the highest resolution stream
+        highest_resolution_url = yt.streams.get_highest_resolution().url
+        highest_resolution_size = yt.streams.get_highest_resolution().filesize
+
+        video_urls_and_sizes = [
+            {
+                "resolution": stream.resolution,
+                "url": stream.url,
+                "size": convert_bytes(stream.filesize)
+            }
+            # for stream in yt.streams.filter(progressive=True,file_extension="mp4") if stream.resolution is not None
+            for stream in yt.streams.filter(progressive=True) if stream.resolution is not None
+        ]
+
+        video_details = {
+            "title": yt.title,
+            "duration": yt.length,
+            "views": yt.views,
+            "author": yt.author,
+            "description": yt.description,
+            "thumbnail_url": yt.thumbnail_url,
+            "video_url": highest_resolution_url,
+            "highest_resolution_size": convert_bytes(highest_resolution_size),
+            "video_urls_by_resolution": video_urls_and_sizes,
+        }
+
+        return video_details
+
+    except Exception as e:
+        return {"error":str(e)}
+
+
+
+@app.route("/api/v1/youtube")
+def youtube_endpoint():
+    try:
+        url = request.args.get("url")
+        if not url:
+            return jsonify({
+                'status': False,
+                'message': 'Url not found.'
+            }), 400    
+        response = get_youtube_detail(url)
+        return jsonify(response)
+
+    except Exception as e:
+        return jsonify({
+            'error': str(e)
+        }), 400
