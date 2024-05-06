3d64d750eeaa366348b7c34a876c7bef532b7c29
Usman Shabbir <ushabbir.se@gmail.com>
2024-05-06 17:37:59 +0500

    updated code

--- app.py
+++ app.py
@@ -1,8 +1,33 @@
+
 import re
-import requests
-from . import bp
-from .helpers import *
-from flask import request, jsonify, send_file
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from flask import Flask, request, jsonify
+
+bp = Flask(__name__)
+
+
+def get_instagram_detail(url):
+    try:
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-gpu")
+
+        browser = webdriver.Chrome(options=chrome_options)
+        browser.get(url)
+        video = WebDriverWait(browser, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//video[@src]"))
+        )
+        video_url = video.get_attribute("src")
+        return {
+            "video_url": video_url,
+        }
+    except Exception as e:
+        print("Exception is : ", e)
+        return        
 
 
 @bp.route("/api/v1/instagram")
